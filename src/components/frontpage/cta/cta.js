467074f42e7c895d707d92e79ab148744dad8546
Mike B. K <mikekristiansen88@gmail.com>
2024-03-07 10:11:26 +0100

    Navbar basics
    added styling to the navbar

---
+++ src/components/frontpage/cta/cta.js
@@ -0,0 +1,7 @@
+import styles from './cta.module.css'
+
+const Cta = async () => {
+  return <div className={styles.container}></div>
+}
+
+export default Cta
--- src/components/navigation/navbar/navbar.js
+++ src/components/navigation/navbar/navbar.js
@@ -1,7 +1,33 @@
-import styles from './navBar.module.css'
+import Image from 'next/image'
+import styles from './navbar.module.css'
+import { FaBars, FaBagShopping } from 'react-icons/fa6'
+import Link from 'next/link'
 
 const NavBar = () => {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.navBarContainer}>
+      <header className={styles.headerWrapper}>
+        <nav className={styles.navWrapper}>
+          {/* Logo  */}
+          <div className={styles.logo}>
+            <Link href='/'>
+              <Image
+                src='/logo/smuknu_logo.png'
+                alt='logo'
+                width={4989}
+                height={1349}
+              />
+            </Link>
+          </div>
+          {/* Right menu items */}
+          <div className={styles.rightMenuItems}>
+            <FaBagShopping />
+            <FaBars />
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
 }
 
 export default NavBar
